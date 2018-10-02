82455484e349f3cdb2f18be45763c4d1d1b03d56
Robbie Jaeger <jaeger.rob@gmail.com>
2018-10-02 15:25:10 -0600

    Add compass image rotation with simulated heading reading

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,23 @@
 (() => {
-  console.log('Script loaded.');
+  const headingReading = document.querySelector('#heading-reading');
+  const compassImg = document.querySelector('#compass img');
+
+  const initSensor = () => {
+    // Start sensor reading from magnetometer
+    setInterval(() => {
+      let heading = Math.floor(Math.random() * 360);
+      updateHeadingReading(heading);
+      rotateCompass(heading);
+    }, 2000);
+  };
+
+  const updateHeadingReading = heading => {
+    headingReading.innerText = heading;
+  };
+
+  const rotateCompass = heading => {
+    compassImg.style.transform = `rotate(${heading}deg)`;
+  };
+
+  initSensor();
 })();
