f4402acea2c488e9d02ae4bc2d0ced0536fdc6d1
Robbie Jaeger <jaeger.rob@gmail.com>
2018-10-02 21:36:49 -0600

    Add magnetometer code

--- public/scripts.js
+++ public/scripts.js
@@ -1,14 +1,51 @@
 (() => {
   const headingReading = document.querySelector('#heading-reading');
   const compassImg = document.querySelector('#compass img');
+  const errorDisplay = document.querySelector('#error-display');
+
+  const displayError = text => {
+    errorDisplay.style = 'display: block;';
+    errorDisplay.innerText = text;
+  };
+  
+  // // For initial compass display testing
+  // const initSensor = () => {
+  //   // Start sensor reading from magnetometer
+  //   setInterval(() => {
+  //     let heading = Math.floor(Math.random() * 360);
+  //     updateHeadingReading(heading);
+  //     rotateCompass(heading);
+  //   }, 1000);
+  // };
 
   const initSensor = () => {
-    // Start sensor reading from magnetometer
-    setInterval(() => {
-      let heading = Math.floor(Math.random() * 360);
-      updateHeadingReading(heading);
-      rotateCompass(heading);
-    }, 2000);
+    magSensor.addEventListener('error', handleError);
+    magSensor.addEventListener('reading', determineHeading);
+    startSensorReading(magSensor);
+  };
+
+  const determineHeading = () => {
+    let x = magSensor.x;
+    let y = magSensor.y;
+    let z = magSensor.z;
+
+    console.log('x:', x, 'y:', y, 'z:', z);
+  };
+
+  const handleError = event => {
+    if (event.error.name === 'NotReadableError') {
+      displayError('Magnetometer not available on this device.');
+    } else if (event.error.name === 'NotAllowedError') {
+      displayError('Permission to access sensor was denied.');
+    }
+  };
+
+  const startSensorReading = sensor => {
+    sensor.start();
+  };
+
+  const stopSensorReading = sensor => {
+    sensor.stop();
   };
 
   const updateHeadingReading = heading => {
@@ -19,5 +56,12 @@
     compassImg.style.transform = `rotate(-${heading}deg)`;
   };
 
-  initSensor();
+  try {
+    const magSensor = new Magnetometer({ frequency: 10 });
+    initSensor();
+  }
+  catch (error) {
+    console.error(error);
+    displayError('Magnetometer not available on this device.');
+  }
 })();
